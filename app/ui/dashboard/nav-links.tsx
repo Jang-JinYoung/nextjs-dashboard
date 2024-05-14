93db526b30f0909f193eb5d1e29fa62babcb73c7
장진영 <jyjang@openit.co.kr>
2024-05-14 09:58:17 +0900

    feat : links 주석처리

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,7 +1,5 @@
 'use client';
  
-import clsx from 'clsx';
-import Link from 'next/link';
 import { usePathname } from 'next/navigation';
  
 // ...
@@ -23,24 +21,26 @@ export default function NavLinks() {
  
   return (
     <>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
-        return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-              {
-                'bg-sky-100 text-blue-600': pathname === link.href,
-              },
-            )}
-          >
-            {/* <LinkIcon className="w-6" /> */}
-            <p className="hidden md:block">{link.name}</p>
-          </Link>
-        );
-      })}
+      {
+      // links.map((link) => {
+      //   const LinkIcon = link.icon;
+      //   return (
+      //     <Link
+      //       key={link.name}
+      //       href={link.href}
+      //       className={clsx(
+      //         'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+      //         {
+      //           'bg-sky-100 text-blue-600': pathname === link.href,
+      //         },
+      //       )}
+      //     >
+      //       {/* <LinkIcon className="w-6" /> */}
+      //       <p className="hidden md:block">{link.name}</p>
+      //     </Link>
+      //   );
+      // })
+      }
     </>
   );
 }
